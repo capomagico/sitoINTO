7c28818bb51b94b6d65a0bbd2c253857b2502c8e
Capomagico <matty.cap@live.it>
2024-07-11 14:48:57 +0200

    prova1

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,55 @@
-<p>svelte-cms home page!</p>
-
-<style>
-	p {
-		font-family: lyon;
-		font-size: 40px;
+<script>
+	import { onMount } from "svelte";
+	import * as faceapi from "face-api.js";
+  
+	let video;
+	let mood = "Unknown";
+  
+	async function loadModels() {
+	  await faceapi.nets.tinyFaceDetector.loadFromUri("/models");
+	  await faceapi.nets.faceExpressionNet.loadFromUri("/models");
 	}
-</style>
+  
+	async function detectMood() {
+	  const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceExpressions();
+	  if (detections.length > 0) {
+		mood = detections[0].expressions.asSortedArray()[0].expression;
+	  }
+	}
+  
+	onMount(async () => {
+	  await loadModels();
+	  navigator.mediaDevices
+		.getUserMedia({ video: true })
+		.then((stream) => {
+		  video.srcObject = stream;
+		  video.play();
+		})
+		.catch((err) => {
+		  console.error("Error accessing webcam:", err);
+		});
+  
+	  setInterval(detectMood, 1000);
+	});
+  </script>
+  
+  <main>
+	<h1>Mood Decoder</h1>
+	<video bind:this={video} width="640" height="480" autoplay></video>
+	<p>Mood: {mood}</p>
+  </main>
+  
+  <style>
+	main {
+	  display: flex;
+	  flex-direction: column;
+	  align-items: center;
+	}
+	video {
+	  margin-top: 20px;
+	}
+	p {
+	  margin-top: 20px;
+	  font-size: 1.5em;
+	}
+  </style>
